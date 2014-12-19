714e8314480d776cf4404c07c313ba9373f25a8c
Daniel Moreau <dr_moreau@comcast.net>
2014-12-19 12:09:07 -0500

    Ipmlemented the two methode makeCacheMatrix and cacheSolve.  cacheSolve will output a message in both cases, either when the value has been cached or when it must be calculated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions demonstrates caching of expensive calculations using
+## R's lexical scoping and the special <<- operator
 
-## Write a short comment describing this function
+## makeCacheMatrix will take a matrix as an argument and acts as an object to
+## store the matrix as well as the calculated inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() { x }
+  setinverse <- function(inverse) {
+    i <<- inverse
+  }
+  getinverse <- function() { i }
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes the makeCacheMatrix function as an argument and returns
+## the iverse of the matrix contined therein.  If the inverse has already been
+## calculated it will return the cached copy, otherwise it will calculate it
+## and cache the result
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("Inverse is cached ... returning cached copy")
+    return(i)
+  }
+  message("Inverse is NOT cached ... returning calculated inverse")
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
